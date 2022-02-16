e6fa8d2d40aee86718cb1967987885b753a265c1
Capzera <1786126188@qq.com>
2022-02-16 19:58:41 +0800

    2022-2-16 alter

--- BlueBridgeCompetitive/OpenJudge/BASIC/BASIC-2.cpp
+++ BlueBridgeCompetitive/OpenJudge/BASIC/BASIC-2.cpp
@@ -1,12 +1,28 @@
 /*
 问题描述：
-
+对于长度为5位的一个01串，每一位都可能是0或1，一共有32种可能。它们的前几个是：
+00000
+00001
+00010
+00011
+00100
+请按从小到大的顺序输出这32种01串。
 */
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    for(int i=0;i<32;i++)
+    {
+        string ans="";
+        ans.push_back(i%2+48);
+        ans.push_back(i/2%2+48);
+        ans.push_back(i/2/2%2+48);
+        ans.push_back(i/2/2/2%2+48);
+        ans.push_back(i/2/2/2/2%2+48);
+        reverse(ans.begin(),ans.end());
+        cout<<ans<<endl;
+    }
     system("pause");
     return 0;
 }
--- BlueBridgeCompetitive/OpenJudge/BASIC/BASIC-8.cpp
+++ BlueBridgeCompetitive/OpenJudge/BASIC/BASIC-8.cpp
@@ -1,12 +1,27 @@
 /*
 问题描述：
-
+　　1221是一个非常特殊的数，它从左边读和从右边读是一样的，编程求所有这样的四位十进制数。
+输出格式
+　　按从小到大的顺序输出满足条件的四位十进制数。
 */
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    vector<int> ve(2);
+    ve.clear();
+    for(int i=10;i<=99;i++)
+    {
+        string ans="";
+        ve.push_back(i%10);
+        ve.push_back(i/10%10);
+        ans+=ve[1]+48;
+        ans+=ve[0]+48;
+        ans+=ve[0]+48;
+        ans+=ve[1]+48;
+        cout<<ans<<endl;
+        ve.clear();
+    }
     system("pause");
     return 0;
 }
