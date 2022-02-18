040b2e56dd74540d12d0357a52f0c6d7f5c4bd39
Capzera <1786126188@qq.com>
2022-02-18 20:02:19 +0800

    2022-2-18

--- Algorithm/Dynamic Programming/LCP1567Maximum Length of Subarray With Positive Product.cpp
+++ Algorithm/Dynamic Programming/LCP1567Maximum Length of Subarray With Positive Product.cpp
@@ -17,3 +17,33 @@
 输出：2
 解释：乘积为正数的最长子数组是 [-1,-2] 或者 [-2,-3] 。
 */
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int length = nums.size();
+        vector<int> positive(length), negative(length);
+        if (nums[0] > 0) {
+            positive[0] = 1;
+        }
+        else if (nums[0] < 0) {
+            negative[0] = 1;
+        }
+        int maxLength = positive[0];
+        for (int i = 1; i < length; ++i) {
+            if (nums[i] > 0) {
+                positive[i] = positive[i - 1] + 1;
+                negative[i] = (negative[i - 1] > 0 ? negative[i - 1] + 1 : 0);
+            }
+            else if (nums[i] < 0) {
+                positive[i] = (negative[i - 1] > 0 ? negative[i - 1] + 1 : 0);
+                negative[i] = positive[i - 1] + 1;
+            }
+            else {
+                positive[i] = 0;
+                negative[i] = 0;
+            }
+            maxLength = max(maxLength, positive[i]);
+        }
+        return maxLength;
+    }
+};
--- LeeCode/P1567.cpp
+++ LeeCode/P1567.cpp
@@ -17,3 +17,64 @@
 输出：2
 解释：乘积为正数的最长子数组是 [-1,-2] 或者 [-2,-3] 。
 */
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int n=nums.size(),i=0,ans=0,minus=0,left=n,right=-1;//贪心算法
+        while (nums[i]==0) 
+        {i++;if (i==n) return 0;}//去除前导0
+        while (nums[n-1]==0) n--;//去除末尾0
+        nums.resize(n+1);nums[n]=0;//补末尾0防止出错
+        int last=i;
+        for(;i<=n;i++)
+        {
+            if (nums[i]==0)//找到0为分隔符
+            {
+                if (minus%2==0) ans=max(ans,i-last);//区间内有偶数个负数，直接计算区间长度
+                else ans=max(ans,max(right-last,i-1-left));
+                last=i+1;minus=0;//下一个起始点为0后
+                right=i;left=n;//重置指针
+                continue;
+            }
+            if (nums[i]<0)
+            {
+                minus++;
+                left=min(left,i);
+                right=max(right,i);
+            }
+        }
+        return ans;
+    }
+};
+/*
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int length = nums.size();
+        vector<int> positive(length), negative(length);
+        if (nums[0] > 0) {
+            positive[0] = 1;
+        }
+        else if (nums[0] < 0) {
+            negative[0] = 1;
+        }
+        int maxLength = positive[0];
+        for (int i = 1; i < length; ++i) {
+            if (nums[i] > 0) {
+                positive[i] = positive[i - 1] + 1;
+                negative[i] = (negative[i - 1] > 0 ? negative[i - 1] + 1 : 0);
+            }
+            else if (nums[i] < 0) {
+                positive[i] = (negative[i - 1] > 0 ? negative[i - 1] + 1 : 0);
+                negative[i] = positive[i - 1] + 1;
+            }
+            else {
+                positive[i] = 0;
+                negative[i] = 0;
+            }
+            maxLength = max(maxLength, positive[i]);
+        }
+        return maxLength;
+    }
+};
+*/
