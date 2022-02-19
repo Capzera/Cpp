8ea86c0815352969a58d33040506b03e610fd7a5
Capzera <1786126188@qq.com>
2022-02-19 18:41:22 +0800

    2022-2-19 alter

--- Algorithm/Dynamic Programming/LCP0714Best Time To Buy and Sell Stock With Transcation Fee.cpp
+++ Algorithm/Dynamic Programming/LCP0714Best Time To Buy and Sell Stock With Transcation Fee.cpp
@@ -30,7 +30,7 @@ public:
         for(i=1;i<n;i++)
         {
             dp[i][0]=max(dp[i-1][1]+prices[i],dp[i-1][0]);//dp[i][0]=dp[i-1][1]在i处卖出和dp[i-1][0]的最大值
-            dp[i][1]=max(dp[i-1][0]-prices[i]-fee,dp[i-1][1]);//dp[i][1]=dp[i-1][-]在i处买入和dp[i-1][1]的最大值
+            dp[i][1]=max(dp[i-1][0]-prices[i]-fee,dp[i-1][1]);//dp[i][1]=dp[i-1][1]在i处买入和dp[i-1][1]的最大值
         }
         return dp[n-1][0];
     }
