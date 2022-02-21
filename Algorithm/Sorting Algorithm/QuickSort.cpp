c2ccbf1bed19086455c45eaedc748c1aeffae941
Capzera <1786126188@qq.com>
2022-02-21 17:19:02 +0800

    2022-2-21 alter

--- Algorithm/Sorting Algorithm/QuickSort.cpp
+++ Algorithm/Sorting Algorithm/QuickSort.cpp
@@ -5,17 +5,17 @@ void QSort(int l,int r,int a[])
     int i=l,j=r,mid=(l+r)/2;
     while(i<=j)
     {
-        while(a[i]<a[mid]) i++;
-        while(a[j]>a[mid]) j--;
-        if (i<=j) 
+        while(a[i]<a[mid]) i++;//将左半边都置为较小值
+        while(a[j]>a[mid]) j--;//将右半边都置为较大值
+        if (i<=j) //发现右边应该是较小值左边应该是较大值
         {
-            swap(a[i],a[j]);
+            swap(a[i],a[j]);//交换
             i++;
             j--;
         }
     }
-    if (l<j) QSort(l,j,a);
-    if (i<r) QSort(i,r,a);
+    if (l<j) QSort(l,j,a);//继续对左边快排
+    if (i<r) QSort(i,r,a);//继续对右边快排
 }
 int main(){
     int a[10001],n,i;
