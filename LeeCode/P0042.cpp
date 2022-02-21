f7c1d71a2e315e61f4c2c98bc22b1dc8b014c346
Capzera <1786126188@qq.com>
2022-02-21 21:35:59 +0800

    2022-2-21 alter

--- LeeCode/P0042.cpp
+++ LeeCode/P0042.cpp
@@ -13,6 +13,7 @@ n == height.length
 1 <= n <= 2 * 104
 0 <= height[i] <= 105
 */
+//双指针
 class Solution {
 public:
     int trap(vector<int>& height) {
@@ -32,6 +33,33 @@ public:
                 ans+=maxright-height[right];
                 right--;
             }
+        }
+        return ans;
+    }
+};
+//动态规划
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> leftMax(n);
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; ++i) {
+            leftMax[i] = max(leftMax[i - 1], height[i]);
+        }
+
+        vector<int> rightMax(n);
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            rightMax[i] = max(rightMax[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans += min(leftMax[i], rightMax[i]) - height[i];
         }
         return ans;
     }
