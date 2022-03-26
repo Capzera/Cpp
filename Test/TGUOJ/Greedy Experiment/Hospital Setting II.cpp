cb71073f9c9fa42c0c0354133068f48da4089f6e
Capzera <1786126188@qq.com>
2022-03-26 13:05:45 +0800

    2022-3-26 alter

--- Test/TGUOJ/Greedy Experiment/Hospital Setting II.cpp
+++ Test/TGUOJ/Greedy Experiment/Hospital Setting II.cpp
@@ -3,65 +3,24 @@ using namespace std;
 class Graph{
 public:
     Graph(int);
-    bool findver(int&);
-    bool insedge(int,int,int);
     void hospital();
 private:
     int vnum;//点数
-    int eNum;//边数
-    vector<int> vertex;//保存顶点
     vector<vector<int>> edge;//保存边
-    bool getverpoint(int&,int&);//判断顶点是否出现
 };
-
-bool Graph::getverpoint(int& vertex,int& i){
-        for (i=0;i<vnum;i++) {
-            if (vertex==this->vertex[i]) return true;
-        }
-        return false;
-}
-
 Graph::Graph(int n){
-    int e;
     int st,ed,wt;
     this->vnum=n;
-    this->vertex=vector<int>(n);
-    this->eNum=0;
-    for (int i=0;i<n;i++) {
-        vertex[i]=i+1;
-    }
-    this->edge=vector<vector<int>> (n,vector<int>(n));
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n;j++) {
-            if (i!=j) edge[i][j]=100000;
-        }
-    }
+    this->edge=vector<vector<int>> (n,vector<int>(n,100000));
     while (1){
         cin>>st>>ed>>wt;
         if (st+ed+wt==0) break;
-        insedge(st,ed,wt);
+        edge[st-1][ed-1]=min(edge[st-1][ed-1],wt);
+        edge[ed-1][st-1]=min(edge[ed-1][st-1],wt);
     }
 }
-
-bool Graph::findver(int& vertex){
-    for (int i=0;i<vnum;i++) {
-        if (vertex==this->vertex[i]) return true;
-    }
-    return false;
-}
-
-bool Graph::insedge(int st,int ed,int wt){
-    int j1=0,j2=0;
-    if (getverpoint(st,j1)&&getverpoint(ed,j2)){
-        edge[j1][j2]=edge[j2][j1]=wt;
-        this->eNum+=2;
-        return true;
-    }
-    return false;
-}
-
 void Graph::hospital(){
-    int i,j,k,s;
+    int i,j,k;
     for (k=0;k<vnum;k++) {
         for (i=0;i<vnum;i++) {
             for (j=0;j<vnum;j++) {
@@ -80,7 +39,6 @@ void Graph::hospital(){
     }
     cout<<minpoint<<endl;
 }
-
 int main(){
     int n;
     cin>>n;
