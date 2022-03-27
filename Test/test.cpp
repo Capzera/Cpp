34f13a0c8635b3ee29e92a66f2ef351c1fbc70f6
Capzera <1786126188@qq.com>
2022-03-27 13:04:02 +0800

    2022-3-27 alter

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,10 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
-	int year;
-	cin>>year;
-	if ((!year%4&&year%100)||(!year%400)) cout<<"Run";
-	else cout<<"Ping";
+	int len;
+	cin>>len;
+	int half=len/2,j=0;
+	if(len%2==0) {
+		int MAX=9*pow(10,half-1);
+		int i=pow(10,half-1);
+		while (j<MAX){
+			string s1=to_string(i),s2(s1);
+			reverse(s2.begin(),s2.end());
+			s1+=s2;
+			cout<<j+1<<" "<<s1<<endl;
+			j++;
+			i+=pow(10,half-2);
+		}
+	}
+	else {
+		half++;
+		int MAX=9*pow(10,half-1);
+		int i=pow(10,half-1);
+		while (j<MAX){
+			string s1=to_string(i),s2;
+			s2=s1.substr(0,half-1);
+			reverse(s2.begin(),s2.end());
+			s1+=s2;
+			cout<<j+1<<" "<<s1<<endl;
+			j++;
+			i++;
+		}
+	}
+	system("pause");
+	return 0;
 }
 /*
 返回值类型 函数名(传入参数1类型 传入参数1，传入参数2类型 传入参数2 ……){
@@ -22,3 +49,12 @@ int main() {
 
 //ProblemA:  键盘输入a,b，输出2*a+b*b;
 //ProblemB:  键盘输入一个正整数n，求1-n的和
+100 100 100 100 100 100 700
+						1
+						1
+						1
+						1
+						1
+						1
+100  8
+1	 7
