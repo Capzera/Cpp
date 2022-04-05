573e0f8637a7a4c41be1975bbe99c176e681a7d2
Capzera <1786126188@qq.com>
2022-04-05 20:23:48 +0800

    2022-4-5 alter

--- Algorithm/Segment Tree/Template.cpp
+++ Algorithm/Segment Tree/Template.cpp
@@ -1,80 +1,63 @@
 #include<bits/stdc++.h>
-#define MAX 10000
 using namespace std;
-int a[MAX],ans[MAX<<2],lazy[MAX<<2],n;
-void pushup(int rt){
-	ans[rt] = ans[rt<<1] + ans[rt<<1|1];	//查询区间和
-	//ans[rt] = max(ans[rt<<1],ans[rt<<1|1]);	查询区间最值
-}
-// build（1，n, 1)
-void build(int l,int r,int rt){
-	if(l == r){
-		ans[rt] = a[l]; //或初始化为0
-		return;
+class SegmentTree {
+public:
+	SegmentTree(vector<int>& nums) : n(nums.size()),Tree(nums.size()*4){
+		build(0,0,n-1,nums);
 	}
-	int mid = (l + r) >> 1;
-	build(l, mid, rt<<1);	//左子树递归
-	build(mid+1, r, rt<<1|1);	//右子树递归
-	pushup(rt);
-}
 
-void pushdown(int rt, int l){
-	if(lazy[rt]){
-		lazy[rt << 1]+=lazy[rt];
-		lazy[rt << 1|1]+=lazy[rt];
-		ans[rt << 1]+=lazy[rt]*(l-l/2);
-		ans[rt << 1|1]+=lazy[rt]*(l/2);
-		lazy[rt]=0;
+	void update(int index,int val) {
+		change(index,val,0,0,n-1);
 	}
-}
 
-void add(int L,int R,int l,int C,int rt){
-	if(L==R)
-    {
-		ans[rt]=C;
-		return;
+	void sumRange(int l,int r){
+		cout<<range(l,r,0,0,n-1)<<endl;
 	}
-	int mid=(L+R)>>1;
-	pushdown(rt,R-L+1);
-	if(l <= mid) add(L,mid,l,C,rt<<1);
-	else add(mid+1,R,l,C,rt<<1|1);
-	pushup(rt);
-}
+private:
+	vector<int> Tree;
+	int n;
+	void build(int node,int l,int r,vector<int> &nums){
+		if (l==r) {
+			Tree[node]=nums[l];
+			return;
+		}
+		int mid=l+(r-l)/2;
+		build(node*2+1,l,mid,nums);
+		build(node*2+2,mid+1,r,nums);
+		Tree[node]=Tree[node*2+1]+Tree[node*2+2];
+	}
 
-void update(int L,int R,int C, int l, int r, int rt){
-	if(L>=l&&R<=r)
-    {
-		ans[rt]=C*(R-L+1);
-		lazy[rt]=C;
-		return;
+	void change(int index,int val,int node,int l,int r){
+		if (l==r) {
+			Tree[node]=val;
+			return;
+		}
+		int mid=l+(r-l)/2;
+		if (index<=mid) change(index,val,node*2+1,l,mid);
+		else change(index,val,node*2+2,mid+1,r);
+		Tree[node]=Tree[node*2+1]+Tree[node*2+2];
 	}
-	int mid = (L+R) >> 1;
-	pushdown(rt, R-L+1);
-	if(r<=mid) update(L, mid, C, l, r, rt<<1);
-	else if(r>mid) update(mid+1, R, C, l, r, rt<<1|1);
-	else
-    {
-		update(L, mid, C, l, r, rt<<1);
-		update(mid+1, R, C ,l, r, rt<<1|1);
-	}
-	pushup(rt);
-}
 
-int query(int L, int R, int l, int r, int rt){
-	if(L >= l && R <= r)
-	return ans[rt];
-	int mid = (L+R)>>1;
-	pushdown(rt, R-L+1);
-	if(r <= mid)
-	return query(L, mid, l, r, rt<<1);
-	else if(l > mid)
-	return query(mid+1, R, l, r, rt<<1|1);
-	else return query(L,mid,l,mid,rt<<1)+query(mid+1,R,mid+1,r,rt<<1|1);
-	//查询区间最值
-	//return max(query(L, mid, l, mid, rt<<1),query(mid+1, R, mid+1, r, rt<<1|1));
-}
-int main()
-{
-    system("pause");
+	int range(int l,int r,int node,int left,int right) {
+		if (l==left&&r==right) return Tree[node];
+		int mid=left+(right-left)/2;
+		if (r<=mid) return range(l,r,node*2+1,left,mid);
+		else if (l>mid) return range(l,r,node*2+2,mid+1,right);
+		else return range(l,mid,node*2+1,left,mid)+range(mid+1,r,node*2+2,mid+1,right);
+	}
+};
+int main(){
+	int n,q;
+	cin>>n>>q;
+	vector<int> nums(n);
+	for (int i=0;i<n;i++) cin>>nums[i];
+	SegmentTree T(nums);
+	while (q--) {
+		int c,x,y;
+		cin>>c>>x>>y;
+		if (c==1) T.update(x,y);
+		else T.sumRange(x,y);
+	}
+	system("pause");
 	return 0;
 }
---
+++ Statement/String/KMP.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+class KMP{
+public:
+    KMP(string s,string t):s(s),t(t),len1(s.size()),len2(t.size()){
+	    GetNextval(t);
+    }
+    void GetNextval(string t)//获取nextval数组
+    {
+        nextval=vector<int>(t.size());//定义一个长度为t-1的数组
+	    nextval[0]=-1;//定义初始边界
+	    for (int i=1,j=0;i<len2;i++) {//遍历t字符串，从1开始，到t-2结束
+            if (j==-1||t[i]==t[j]) {//j==-1说明没有公共前后缀，t[i]==t[j]当前两个指针指向的量相同
+                j++;
+                if (t[i]!=t[j]) nextval[i]=j;//nextval直接推对应在前缀后面的下标
+                else nextval[i]=nextval[j];//nextval直接往右拉
+            }
+            else j=nextval[j];//需要回溯
+	    }
+    } 
+    int KMPindex(string s,string t) {//求KMP算法
+        int i=0,j=0;
+	    while (i<len1&&j<len2) {
+		    if (j==-1||s[i]==t[j]) {
+                i++;
+                j++;
+            }
+		    else j=nextval[j];
+        }
+        if (j>=len2) return(i-len2);
+        else  return(-1);
+    }
+    void show(){//打印函数
+        int val=KMPindex(s,t);
+        if (val<0) cout<<"未匹配"<<endl;
+        else cout<<"匹配索引为"<<val<<"--"<<val+len2-1<<endl;
+    }
+private:
+    string s,t;
+    int len1,len2;
+    vector<int> nextval;
+};
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    KMP K(s1,s2);
+    K.show();
+    system("pause");
+    return 0;
+}
