430a13391ccd121d5d74306b924a8c474d7b7e34
Capzera <1786126188@qq.com>
2022-04-15 19:19:18 +0800

    2022-4-15 alter

--- Algorithm/Sorting Algorithm/QuickSort.cpp
+++ Algorithm/Sorting Algorithm/QuickSort.cpp
@@ -1,28 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-void QSort(int l,int r,int a[])
+vector<int> nums;
+void QSort(int l,int r)
 {
-    int i=l,j=r,mid=(l+r)/2;
-    while(i<=j)
-    {
-        while(a[i]<a[mid]) i++;//将左半边都置为较小值
-        while(a[j]>a[mid]) j--;//将右半边都置为较大值
-        if (i<=j) //发现右边应该是较小值左边应该是较大值
-        {
-            swap(a[i],a[j]);//交换
+    if (l>=r) return;
+    int i=l,j=r,mid=nums[(l+r)/2];
+    while(i<=j) {
+        while(nums[i]<mid) i++;
+        while(nums[j]>mid) j--;
+        if (i<=j) {
+            swap(nums[i],nums[j]);
             i++;
             j--;
         }
     }
-    if (l<j) QSort(l,j,a);//继续对左边快排
-    if (i<r) QSort(i,r,a);//继续对右边快排
+    QSort(i,r);
+    QSort(l,j);
 }
 int main(){
-    int a[10001],n,i;
+    int n,i;
     cin>>n;
-    for(i=1;i<=n;i++) cin>>a[i];
-    QSort(1,n,a);
-    for(i=1;i<=n;i++) cout<<a[i]<<" ";
+    nums.resize(n);
+    for(i=0;i<n;i++) cin>>nums[i];
+    QSort(0,n-1);
+    for(i=0;i<n;i++) cout<<nums[i]<<" ";
     system("pause");
     return 0;
 }
