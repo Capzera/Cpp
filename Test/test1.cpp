611e926ac233785886c12e0e43dcb35c32b894bb
Capzera <1786126188@qq.com>
2022-05-08 20:39:05 +0800

    2022-5-8 alter

--- Test/test1.cpp
+++ Test/test1.cpp
@@ -1,31 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int t,n,a,b;
-	string s;
-	cin>>t;
-	for (int T=0;T<t;T++) {
-		cin>>s;
-		n=s.size();
-		int ans=0,i=0,j=1;
-		while (i<n&&j<n) {
-			while (j<n&&s[j]!=s[i]) j++;
-			if (j<n) {
-				ans+=j-i-1;
-				i=j+1;
-				j=i+1;
-			}
-			else {
-				i++;
-				ans++;
-				j=i+1;
-			}
-		}
-		cout<<ans<<endl;
-	}
-	system("pause");
-    return 0;
+int gcd(int m,int n){
+	if (m%n==0) return n;
+	return gcd(n,m%n);
 }
-obbo
-dp[i][j]表示第i个字符到第j个字符需要删除最少的个数就能满足偶字符串的个数
-dp[i][j]=max(dp[i+1][j-1]+2,)
+int main(){
+	int a,b;
+	cin>>a>>b;
+	if (a<b) swap(a,b);
+	cout<<gcd(a,b)<<endl;
+	system("pause");
+	return 0;
+}
