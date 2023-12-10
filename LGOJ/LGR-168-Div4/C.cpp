d08407bdc835691196032af0198bf1f2850e2d8c
Capzera <1786126188@qq.com>
2023-12-10 11:29:53 +0800

    2023-12-10

--- LGOJ/LGR-168-Div4/C.cpp
+++ LGOJ/LGR-168-Div4/C.cpp
@@ -4,9 +4,8 @@ int main() {
     int n, k;
     cin >> n >> k;
     long long mx = pow(n, n), now = n, ans = 0;
-    while (now <= mx) {
-        if (now % k % n == 0) ans++;
-        now += 10;
+    for (int i = n; i <= mx; i += 10) {
+        if (i % k % n == 0) ans++;
     }
     cout << ans << endl;
     system("pause");
